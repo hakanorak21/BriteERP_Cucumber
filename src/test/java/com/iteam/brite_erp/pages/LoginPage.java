e67d705fa66e34cbc679a0d5cdd5400f99c3b419
hakanorak21 <hakanorak21@yahoo.com>
2020-01-07 17:56:13 -0600

    LoginPage fixed and tested BasePage also tested configuration.properties file fixed

--- src/test/java/com/iteam/brite_erp/pages/LoginPage.java
+++ src/test/java/com/iteam/brite_erp/pages/LoginPage.java
@@ -1,65 +1,76 @@
 package com.iteam.brite_erp.pages;
 
+import com.iteam.brite_erp.utilities.BrowserUtils;
+import com.iteam.brite_erp.utilities.ConfigurationReader;
+import com.iteam.brite_erp.utilities.Driver;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import java.sql.Driver;
-
 public class LoginPage extends BasePage {
 
-    @FindBy(xpath = "//a [@href='/web/login']")
+    @FindBy(xpath = "//a[@href='/web/login']/b")
     public WebElement loginLink;
 
-    @FindBy(xpath = "//input[@id='login']")
+    @FindBy(id = "login")
     public WebElement emailBox;
 
-    @FindBy(xpath = "//input[@id='password']")
+    @FindBy(id = "password")
     public WebElement passwordBox;
 
-    @FindBy(xpath = "//button[contains(text(),'Log in')]")
+    @FindBy(css = ".btn.btn-primary")
     public  WebElement loginButton;
 
-    //-------------------------------------------------------------------------------------------------------------
+
     public LoginPage(){
         PageFactory.initElements(Driver.get(), this);
     }
-//--------------------------------------------------------------------------------------------------------------
 
-    // on the firs page we have to click on login link
+
+    // on the first page, we have to click on login link
     // Login method
-    public void login(String email, String password) {
+    public void login(String username, String password) {
+        loginLink.click();
+        BrowserUtils.waitForVisibility(emailBox, 5);
+        emailBox.sendKeys(username);
+        passwordBox.sendKeys(password);
+        BrowserUtils.wait(2);
         loginButton.click();
-        emailBox.sendKeys(email);
-        passwordBox.sendKeys(password, Keys.ENTER);
     }
-//---------------------------------------------------------------------------------------------------------------
 
-    // constructor
+    // By deafult, login as Inventory Manager
     public void login() {
-        login(ConfigurationReader.getProperty("username"), ConfigurationReader.getProperty("password"));
+        login(ConfigurationReader.getProperty("contactsUsername"), ConfigurationReader.getProperty("contactsPassword"));
     }
 
-    //------------------------------------------------------------------------------------------------------------
-
+    /*
+    Roles:
+        Inventory Manager
+        POS Manager
+        Events Manager
+     */
     public void login(String role) {
-        String userName = "";
+        String username = "";
         String password = "";
 
         switch (role) {
-            case "Pos Manager":
-                userName = ConfigurationReader.getProperty("posmanager.username");
-                password = ConfigurationReader.getProperty("posmanager.password");
+            case "Inventory Manager":
+                username = ConfigurationReader.getProperty("contactsUsername");
+                password = ConfigurationReader.getProperty("contactsPassword");
                 break;
-            case "Events CRM Manager":
-                userName = ConfigurationReader.getProperty("eventscrmmanager.username");
-                password = ConfigurationReader.getProperty("posmanager.password");
+            case "POS Manager":
+                username = ConfigurationReader.getProperty("posUsername");
+                password = ConfigurationReader.getProperty("posPassword");
+                break;
+            case "Events Manager":
+                username = ConfigurationReader.getProperty("crmUsername");
+                password = ConfigurationReader.getProperty("crmPassword");
                 break;
             default:
                 throw new RuntimeException("Invalid role!");
         }
-        login(userName, password);
+        login(username, password);
     }
 
 }
